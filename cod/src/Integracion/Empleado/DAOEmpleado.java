11038bd1962dde2370d97b8b98c191152ad83f85
blackseif <83184449+blackseif@users.noreply.github.com>
2021-05-07 19:19:36 +0200

    Update DAOEmpleado.java

--- cod/src/Integracion/Empleado/DAOEmpleado.java
+++ cod/src/Integracion/Empleado/DAOEmpleado.java
@@ -1,5 +1,8 @@
 package Integracion.Empleado;
 
 public interface DAOEmpleado {
-
-}
+  public TEmpleado leerEmpleadoDNI(String dni);
+  public TEmpleado leerEmpleadoNombre(String nombre);
+  public TEmpleado leerEmpleadoApellidos(String apellidos);
+  public List<TEmpleado> leerEmpleados();
+} 
