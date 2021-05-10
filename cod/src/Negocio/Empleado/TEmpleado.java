c27977b47efca511a95eb058b7dfc5e7fb7d4f05
blackseif <83184449+blackseif@users.noreply.github.com>
2021-05-10 16:23:24 +0200

    Update TEmpleado.java

--- cod/src/Negocio/Empleado/TEmpleado.java
+++ cod/src/Negocio/Empleado/TEmpleado.java
@@ -1,5 +1,46 @@
 package Negocio.Empleado;
 
 public class TEmpleado {
-
+	protected String dni;
+	protected String nombre;
+	protected String apellidos;
+	protected String telefono;
+	protected String password;
+	protected boolean activo;
+	
+	public TEmpleado(String dni, String nombre,String apellidos,String telefono, String password) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.telefono = telefono;
+		this.password = password;
+		activo = true;
+	}
+	
+	public String getDNI() {
+		return dni;
+	}
+	public String getPW() {
+		return password;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	
+	public boolean getActivo() {
+		return activo;
+	}
+	
+	public void setActivo(boolean x) {
+		activo = x;
+	}
 }
