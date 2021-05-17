a240111585dbe8b91d265320b698f968e2d4f282
kun-sys <73662477+kun-sys@users.noreply.github.com>
2021-05-17 17:37:37 +0200

    Update FactoriaSA.java

--- cod/src/Negocio/Factoria/FactoriaSA.java
+++ cod/src/Negocio/Factoria/FactoriaSA.java
@@ -1,5 +1,21 @@
 package Negocio.Factoria;
 
+import Negocio.Clientes.SACliente;
+import Negocio.Tarifa.SATarifa;
+import Negocio.Contrato.SAContrato;
+import Negocio.Empleado.SAEmpleado;
+
 public abstract class FactoriaSA {
+  public static FactoriaSA instancia;
+	public static FactoriaSA getInstance() {
+		if(instancia == null) {
+			instancia = new FactoriaSAImp();
+		}
+		return instancia;
+	}
+	public abstract SACliente generaSACliente();
+	public abstract SAEmpleado generaSAEmpleado();
+	public abstract SATarifa generaSATarifa ();
+	public abstract SAContrato generaSAContrato();
 
 }
