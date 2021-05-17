84b94331c5c0b88f2584ba1621fd5963fc1361c8
kun-sys <73662477+kun-sys@users.noreply.github.com>
2021-05-17 17:41:16 +0200

    Update FactoriaSAImp.java

--- cod/src/Negocio/Factoria/Imp/FactoriaSAImp.java
+++ cod/src/Negocio/Factoria/Imp/FactoriaSAImp.java
@@ -1,7 +1,35 @@
 package Negocio.Factoria.Imp;
 
 import Negocio.Factoria.FactoriaSA;
+import Negocio.Clientes.SACliente;
+import Negocio.Clientes.Imp.SAClienteImp;
+import Negocio.Tarifa.SATarifa;
+import Negocio.Tarifa.Imp.SATarifaImp;
+import Negocio.Contrato.SAContrato;
+import Negocio.Contrato.Imp.SAContratoImp;
+import Negocio.Empleado.SAEmpleado;
+import Negocio.Empleado.Imp.SAEmpleadoImp;
 
 public class FactoriaSAImp extends FactoriaSA{
+  	@Override
+	public SACliente generaSACliente() {
+		return new SAClienteImp();
+	}
+
+	@Override
+	public SAEmpleado generaSAEmpleado() {
+		return new SAEmpleadoImp();
+	}
+
+	@Override
+	public SATarifa generaSATarifa() {
+		return new SATarifaImp();
+	}
+
+	@Override
+	public SAContrato generaSAContrato() {
+		return new SAContratoImp();
+	}
+
 
 }
