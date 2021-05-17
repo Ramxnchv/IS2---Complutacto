b64b6a4a0812cd6492bd2021c305c7b4f2b86a24
blackseif <83184449+blackseif@users.noreply.github.com>
2021-05-17 17:46:24 +0200

    Update SAEmpleado.java

--- cod/src/Negocio/Empleado/SAEmpleado.java
+++ cod/src/Negocio/Empleado/SAEmpleado.java
@@ -1,5 +1,8 @@
 package Negocio.Empleado;
 
 public interface SAEmpleado {
-
+	public boolean AltaEmpleado(String dni) throws Exception;
+	public boolean BajaEmpleado(String dni) throws Exception;
+	public TEmpleado mostrarEmpleado(String dni) throws Exception;
+	public Collection<TEmpleado> mostrarTodos() throws Exception;
 }
