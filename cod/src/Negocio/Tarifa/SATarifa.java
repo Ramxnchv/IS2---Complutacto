cddab9461a0d6ef3219e9ef886eb183a3fb4dc6c
kun-sys <73662477+kun-sys@users.noreply.github.com>
2021-05-17 19:04:30 +0200

    Update SATarifa.java

--- cod/src/Negocio/Tarifa/SATarifa.java
+++ cod/src/Negocio/Tarifa/SATarifa.java
@@ -1,38 +1,13 @@
-
 package Negocio.Tarifa;
 
-public class TTarifa {
-	private int id;
-	private String nombre;
-	private double precio;
-	private boolean activo;
-	public TTarifa(int id, String nombre, double precio) {
-		this.id = id;
-		this.nombre = nombre;
-		this.precio = precio;
-		this.activo = true;
-		
-	}
-	public int getID() {
-		return id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public double getPrecio() {
-		return precio;
-	}
-	public boolean getActivo() {
-		return activo;
-	}
-	public void setActivo(boolean activar) {
-		activo = activar;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
+import java.util.Collection;
+
+public interface SATarifa {
+	public boolean anadirTarifa(int id, String nombre, double precio) throws Exception;
+	public boolean borrarTarifa(int id)throws Exception;
+	public boolean modificarTarifa(TTarifa tarifa)throws Exception;
+	public TTarifa mostrarTarifa(int id)throws Exception;
+	public Collection<TTarifa> mostrarTodos()throws Exception;	public Collection<TTarifa> mostrarTarifaPorEditorial(int id)throws Exception;
+
 
 }
