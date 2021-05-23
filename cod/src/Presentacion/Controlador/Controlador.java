45d33a5893c8d3eeb89f98c3694d2821482c61ea
Ramón <ramonchuwii@gmail.com>
2021-05-23 21:33:51 +0200

    Cambios controlador

--- cod/src/Presentacion/Controlador/Controlador.java
+++ cod/src/Presentacion/Controlador/Controlador.java
@@ -1,5 +1,36 @@
 package Presentacion.Controlador;
 
-public abstract class Controlador {
+import Presentacion.Controlador.Imp.ControladorImp;
 
+public abstract class Controlador {
+	public static class Evento{
+		public static final int ALTA_CLIENTE=101;
+		public static final int BAJA_CLIENTE=102;
+		public static final int MODIFICAR_CLIENTE=103;
+		public static final int ACCION_ALTA_CLIENTE=104;
+		public static final int ACCION_BAJA_CLIENTE=105;
+		public static final int ACCION_MODIFICAR_CLIENTE=106;
+		public static final int MOSTRAR_CLIENTE=107;
+		public static final int MOSTRAR_TODOS_CLIENTE=108;
+		
+		public static final int ALTA_EMPLEADO=201;
+		public static final int BAJA_EMPLEADO=202;
+		public static final int MODIFICAR_EMPLEADO=203;
+		public static final int ACCION_ALTA_EMPLEADOL=204;
+		public static final int ACCION_BAJA_EMPLEADO=205;
+		public static final int ACCION_MODIFICAR_EMPLEADO=206;
+		public static final int MOSTRAR_EMPLEADO=207;
+		public static final int MOSTRAR_TODOS_EMPLEADO=208;
+	}
+	
+	private static Controlador instancia;
+	
+	public static Controlador getInstance(){
+		if(instancia == null){
+			instancia = new ControladorImp();
+		}
+		
+		return instancia;
+	}
+	public abstract void accion(int evento, Object object);
 }
--- cod/src/Presentacion/Main/MainWindow.java
+++ cod/src/Presentacion/Main/MainWindow.java
@@ -3,5 +3,14 @@ package Presentacion.Main;
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame{
+	
+	public static void main(String[] args) {
+		MainWindow myWindow=new MainWindow();
+		myWindow.initGUI();
+	}
 
+	private void initGUI() {
+		// TODO Auto-generated method stub
+		
+	}
 }
