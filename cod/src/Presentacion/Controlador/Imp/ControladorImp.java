c4c8d48dee6ef7c692ec96456052a7c8562f2a36
Ramón <ramonchuwii@gmail.com>
2021-06-07 20:50:58 +0200

    Mas cambios en controlador

--- cod/src/Presentacion/Controlador/Imp/ControladorImp.java
+++ cod/src/Presentacion/Controlador/Imp/ControladorImp.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 import Negocio.Cliente.SACliente;
 import Negocio.Cliente.TCliente;
 import Negocio.Empleado.SAEmpleado;
+import Negocio.Empleado.TEmpleado;
 import Negocio.Factoria.FactoriaSA;
 import Presentacion.Controlador.Controlador;
 import Presentacion.Factoria.FactoriaVistas;
@@ -39,7 +40,7 @@ public class ControladorImp extends Controlador{
 				saCliente= factoria.generaSACliente();
 				try {
 					saCliente.BajaCliente(dni);
-					JOptionPane.showMessageDialog(null,  "Cliente dado de alta", "ALTA CLIENTE", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(null,  "Cliente dado de baja", "BAJA CLIENTE", JOptionPane.INFORMATION_MESSAGE);
 				}
 				catch(Exception e) {
 					JOptionPane.showMessageDialog(null, e.getMessage());
@@ -47,28 +48,96 @@ public class ControladorImp extends Controlador{
 				
 			}break;
 			case Evento.MODIFICAR_CLIENTE :{
-				
+				TCliente cliente = (TCliente) object;
+				factoria = FactoriaSA.getInstance();
+				saCliente = factoria.generaSACliente();
+				try {
+					saCliente.ModificarCliente(cliente);
+					JOptionPane.showMessageDialog(null,  "Cliente modificado", "MODIFICAR CLIENTE", JOptionPane.INFORMATION_MESSAGE);
+				}
+				catch(Exception e){
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
 			}break;
 			case Evento.MOSTRAR_CLIENTE :{
-				
+				String dni = (String) object;
+				factoria = FactoriaSA.getInstance();
+				saCliente= factoria.generaSACliente();
+				try {
+					//vistas.generaMostrarClienteGUI().mostrarCliente(saCliente.MostrarCliente(dni));
+				}
+				catch(Exception e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
 			}break;
 			case Evento.MOSTRAR_TODOS_CLIENTE:{
-				
+				factoria = FactoriaSA.getInstance();
+				saCliente= factoria.generaSACliente();
+				//MostrarTodosClientesGUI most= new MostrarTodosClientesGUI();
+				try {
+					//most.MostrarTodosClientes(saCliente.MostrarTodos());
+				}
+				catch(Exception e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
 			}break;
 			case Evento.ALTA_EMPLEADO:{
-				
+				TEmpleado empleado = (TEmpleado) object;
+				factoria = FactoriaSA.getInstance();
+				saEmpleado = factoria.generaSAEmpleado();
+				try {
+//					saEmpleado.AltaEmpleado(empleado);
+					JOptionPane.showMessageDialog(null,  "Empleado dado de alta", "ALTA EMPLEADO", JOptionPane.INFORMATION_MESSAGE);
+				}
+				catch(Exception e){
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
 			}break;
 			case Evento.BAJA_EMPLEADO:{
+				String dni = (String) object;
+				factoria = FactoriaSA.getInstance();
+				saEmpleado= factoria.generaSAEmpleado();
+				try {
+					saEmpleado.BajaEmpleado(dni);
+					JOptionPane.showMessageDialog(null,  "Cliente dado de baja", "BAJA CLIENTE", JOptionPane.INFORMATION_MESSAGE);
+				}
+				catch(Exception e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
 				
 			}break;
 			case Evento.MODIFICAR_EMPLEADO :{
-				
+				TEmpleado empleado = (TEmpleado) object;
+				factoria = FactoriaSA.getInstance();
+				saEmpleado = factoria.generaSAEmpleado();
+				try {
+					//saEmpleado.ModificarEmpleado(empleado);
+					JOptionPane.showMessageDialog(null,  "Empleado modificado", "MODIFICAR EMPLEADO", JOptionPane.INFORMATION_MESSAGE);
+				}
+				catch(Exception e){
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
 			}break;
 			case Evento.MOSTRAR_EMPLEADO :{
-				
+				String dni = (String) object;
+				factoria = FactoriaSA.getInstance();
+				saEmpleado = factoria.generaSAEmpleado();
+				try {
+					//vistas.generaMostrarEmpleadoGUI().mostrarEmpleado(saEmpleado.MostrarEmpleado(dni));
+				}
+				catch(Exception e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
 			}break;
 			case Evento.MOSTRAR_TODOS_EMPLEADO:{
-				
+				factoria = FactoriaSA.getInstance();
+				saEmpleado = factoria.generaSAEmpleado();
+				//MostrarTodosEmpleadosGUI most= new MostrarTodosEmpleadosGUI();
+				try {
+					//most.MostrarTodosEmpleados(saEmpleado.MostrarTodos());
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
 			}break;
 		}
 	}
